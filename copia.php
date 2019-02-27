d794216c2363f560640fa3e8b303b314c1b09b1a
alfilodeloimposible <31593702+alfilodeloimposible@users.noreply.github.com>
2019-02-27 18:54:40 +0100

    Add files via upload

---
+++ copia.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<head><title></title>
+
+<link href="https://vjs.zencdn.net/7.4.1/video-js.css" rel="stylesheet">
+
+  <!-- If you'd like to support IE8 (for Video.js versions prior to v7) -->
+  <script src="https://vjs.zencdn.net/ie8/ie8-version/videojs-ie8.min.js"></script>
+
+</head>
+<body bgcolor="lime">
+
+<video id="my-video" class="video-js" controls preload="auto" width="250" height="160"
+  poster="MY_VIDEO_POSTER.jpg" data-setup="{}">
+    <source src="https://ia601501.us.archive.org/29/items/discotqa_gmail_1.0/TiroLoco.mp4" type="video/mp4">
+    <source src="http://iptvperu@media-es-am.crackle.com/1/v/f0/h7gyk_480p_1mbps.mp4" type="video/mp4">
+    <p class="vjs-no-js">
+      To view this video please enable JavaScript, and consider upgrading to a web browser that
+      <a href="https://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>
+    </p>
+	
+	 </video>
+	 
+	    <br />   <br />   <br />
+		
+	 <video id="my-video" class="video-js" controls preload="auto" width="250" height="160"
+  poster="MY_VIDEO_POSTER.jpg" data-setup="{}">   
+
+    <source src="https://tumarcador.xyz/ac3.php" type="video/mp4">
+    <p class="vjs-no-js">
+      To view this video please enable JavaScript, and consider upgrading to a web browser that
+      <a href="https://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>
+    </p>
+	
+	 </video>
+	<script src='https://vjs.zencdn.net/7.4.1/video.js'></script>
+
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
